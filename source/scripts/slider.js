9f173c2f7c20a70317696267f1b38559b1c6fd35
Dima Tkachuk <35940070+dima-tk@users.noreply.github.com>
2024-09-25 00:50:37 +0300

    стили все

---
+++ source/scripts/slider.js
@@ -0,0 +1,19 @@
+(function () {
+  const slider = document.querySelector('.live__slider');
+  const clientWidth = document.body.clientWidth;
+
+  //if (clientWidth >= 768) {
+  const before = slider.querySelector('.live__slide--before');
+  const after = slider.querySelector('.live__slide--after');
+  const input = slider.querySelector('.live__range');
+  const container = document.querySelector('.live__progress-bar');
+
+  input.addEventListener('input', (e) => {
+    after.style.width = `${100 - input.value }%`;
+    before.style.width = `${input.value }%`;
+    container.style.setProperty('--position', `${e.target.value}%`);
+  });
+  //}
+
+
+})();
